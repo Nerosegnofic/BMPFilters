2fcbc94df3a23b041abd1f357ba8464e58bc897c
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2023-10-16 14:33:16 +0300

    Update CS213-2023-20220027-20220028-20220360-A1-FULL.cpp

--- Greyscale/CS213-2023-20220027-20220028-20220360-A1-FULL.cpp
+++ Greyscale/CS213-2023-20220027-20220028-20220360-A1-FULL.cpp
@@ -843,7 +843,6 @@ void menu() {
              << "12 - Blur Image.\n"
              << "13 - Crop Image.\n"
              << "14 - Skew Image.\n"
-             << "15 - Save Image.\n"
              << "0 - Exit" << endl;
         cin >> choice;
         switch (choice) {
@@ -889,11 +888,25 @@ void menu() {
             case 14:
                 skew();
                 break;
-            case 15:
-                saveImage();
-                break;
             default:
                 break;
+        }
+        
+        if (choice != 0) {
+            cout << "Do you want to save this filter?" << endl;
+            cout << "(Y): yes or (N): no" << endl;
+
+            char choice2;
+            cin >> choice2;
+            choice2 = tolower(choice2);
+
+            if (choice2 == 'y') {
+                saveImage();
+                cout << "Do you want to exit? (y) yes or (n) no" << endl;
+                cin >> choice2;
+                if (choice2 == 'y')
+                    return;
+            }
         }
     } while (choice != 0);
 }
